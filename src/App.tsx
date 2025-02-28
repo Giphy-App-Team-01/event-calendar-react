6b31b0ca08d65ccad811b02fbeda3a643462832b
pyordanovdev <plamenyordanovdev@gmail.com>
2025-02-28 18:48:35 +0200

    initial setup of project structure and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <input
+      type='text'
+      placeholder='Type here'
+      className='input input-bordered w-full max-w-xs'
+    />
+  );
+}
+
+export default App;
---
+++ src/common/constants.ts
@@ -0,0 +1,18 @@
+// User specific
+export const MIN_USERNAME_LENGTH: number = 3;
+export const MAX_USERNAME_LENGTH: number = 30;
+export const MIN_PASSWORD_LENGTH: number = 8;
+export const MAX_PASSWORD_LENGTH: number = 30;
+export const MIN_FIRST_LAST_NAME_LENGTH: number = 1;
+export const MAX_FIRST_LAST_NAME_LENGTH: number = 30;
+export const MIN_PHONE_NUMBER_LENGTH: number = 10;
+export const MIN_CONTACTS_LIST_TITLE_LENGTH: number = 3;
+export const MAX_CONTACTS_LIST_TITLE_LENGTH: number = 30;
+
+// Event specific
+export const MIN_TITLE_LENGTH: number = 3;
+export const MAX_TITLE_LENGTH: number = 30;
+export const MIN_DESCRIPTION_LENGTH: number = 3;
+export const MAX_DESCRIPTION_LENGTH: number = 500;
+export const MAX_COVER_PHOTO_SIZE: number = 5 * 1024 * 1024; // 5 MB
+export const MAX_EVENTS_COUNT_GRID: number = 10;
