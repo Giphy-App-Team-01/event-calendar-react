4c0eb4bf35528daefe90bddce9d09fa190aacc8b
pyordanovdev <plamenyordanovdev@gmail.com>
2025-03-04 14:21:01 +0200

    landing page view changes: added markup

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -2,6 +2,7 @@ import Button from '../../components/Button/Button';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import calendarHome from '../../assets/images/calendar-home.jpeg';
 import { getAllPublicEvents } from '../../services/db-service';
+import SingleEventItemCard from '../../components/SingleEventItemCard/SingleEventItemCard';
 interface HeaderProps {
   setIsLoginOpen: Dispatch<SetStateAction<boolean>>;
   setIsRegisterOpen: Dispatch<SetStateAction<boolean>>;
@@ -25,46 +26,42 @@ const LandingPage: React.FC<HeaderProps> = ({
     fetchPublicEvents();
   }, []);
   return (
-    <div className='LandingPage flex items-center gap-10'>
-      <div className='text-box w-1/2'>
-        <h1 className='text-4xl font-bold text-gray-950 mb-3'>
-          Shareable Online Calendar
-        </h1>
-        <div className='text-lg text-gray-950'>
-          Spend less time planning and more time doing with a shareable calendar
-          that works across your workspace.
+    <>
+      <div className='hero flex items-center gap-10'>
+        <div className='text-box w-1/2'>
+          <h1 className='text-4xl font-bold text-gray-950 mb-3'>
+            Shareable Online Calendar
+          </h1>
+          <div className='text-lg text-gray-950'>
+            Spend less time planning and more time doing with a shareable
+            calendar that works across your workspace.
+          </div>
+          <div className='flex items-center gap-4 mt-6'>
+            <Button
+              onClick={() => setIsLoginOpen(true)}
+              className='bg-cyan-500 hover:bg-cyan-600 text-white transition'
+            >
+              Sign in
+            </Button>
+            <Button
+              onClick={() => setIsRegisterOpen(true)}
+              className='bg-white text-gray-900 hover:bg-gray-200'
+            >
+              Register
+            </Button>
+          </div>
         </div>
-        <div className='flex items-center gap-4 mt-6'>
-          <Button
-            onClick={() => setIsLoginOpen(true)}
-            className='bg-cyan-500 hover:bg-cyan-600 text-white transition'
-          >
-            Sign in
-          </Button>
-          <Button
-            onClick={() => setIsRegisterOpen(true)}
-            className='bg-white text-gray-900 hover:bg-gray-200'
-          >
-            Register
-          </Button>
+        <div className='img-box w-1/2'>
+          <img src={calendarHome} alt='' />
         </div>
       </div>
-      <div className='img-box w-1/2'>
-        <img src={calendarHome} alt='' />
-      </div>
-
       <div className='public-events'>
         {publicEvents &&
           publicEvents.map((event: any) => (
-            <div key={event.id}>
-              <h2>{event.title}</h2>
-              <p>{event.start}</p>
-              <p>{event.end}</p>
-              <p>{event.location}</p>
-            </div>
+            <SingleEventItemCard key={event.id} event={event} />
           ))}
       </div>
-    </div>
+    </>
   );
 };
 
